8ddddd0e774e04714a59280ea0255d544015ede0
PalominoAxel <axelpalomino95@gmail.com>
2026-05-14 21:25:32 -0500

    Add locales data for multiple locations

---
+++ data/locales.js
@@ -0,0 +1,31 @@
+const locales = [
+  {
+    id: 1,
+    nombre: "Miraflores",
+    direccion: "Av. Diagonal 308",
+    horario: "Lunes a Domingo | 8:00 am - 11:00 pm",
+    telefono: "987 654 321",
+    imagen: "assets/img/locales/miraflores.jpg",
+    maps: "https://maps.app.goo.gl/Yz6yMKe6F9J82jYp6"
+  },
+
+  {
+    id: 2,
+    nombre: "Chorrillos",
+    direccion: "Centro Comercial Plaza Lima Sur",
+    horario: "Lunes a Domingo | 9:00 am - 10:00 pm",
+    telefono: "999 999 999",
+    imagen: "assets/img/locales/chorrillos.jpg",
+    maps: "https://maps.app.goo.gl/WMecH8qD9v8tY69v9"
+  },
+
+  {
+    id: 3,
+    nombre: "Barranco",
+    direccion: "Av. Almte. Miguel Grau 302",
+    horario: "Lunes a Domingo | 9:00 am - 10:30 pm",
+    telefono: "956 789 123",
+    imagen: "assets/img/locales/barranco.jpg",
+    maps: "https://maps.app.goo.gl/9PJDogAYziw4nR1V9"
+  }
+];
